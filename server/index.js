4919664bb207bced77222c61cd653997626db3e6
Valentin <romic.valentin@gmail.com>
2023-09-21 09:24:02 +0200

    Konacna verzija

--- server/index.js
+++ server/index.js
@@ -15,8 +15,6 @@ import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
 import { postDelete } from "./controllers/posts.js";
 
 var options = {
@@ -91,9 +89,6 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-    /* ADD DATA ONE TIME */
-    //User.insertMany( users );
-    //Post.insertMany( posts );
 })
 .catch((error) => console.log(`${error} did not connect`));
 
